73ad37a12d3a4c92fa7d439178bc0ccf288d1153
AndyDwi <andydwiprasetyo1994@gmail.com>
2023-05-06 22:27:25 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mobile Programming',
+      title: "Mobile Programing pertemuan 2",
       home: MyHomePage(),
     );
   }
@@ -24,24 +24,33 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mobile Programming'),
+        title: const Text('06TPLM002 - 191011400713'),
       ),
-      body: const Center(
-        child: Text('Andy Dwi Prasetyo'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Kelas : 06TPLM002'),
+            SizedBox(height: 20),
+            Text('Nama : Andy Dwi Prasetyo'),
+            SizedBox(height: 20),
+            Text('NIM : 191011400713')
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
             icon: Icon(Icons.search),
-            label: 'Search',
+            label: 'explore',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
+            icon: Icon(Icons.message),
+            label: 'feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'setting',
           ),
         ],
       ),
